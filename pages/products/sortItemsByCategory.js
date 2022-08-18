da3c043f5f3c1d063feb15cb04145beffd2bbf37
Megumi Kimura <kimuramegumi@gmail.com>
2022-08-18 08:58:19 +0200

    add proxy

--- pages/products/sortItemsByCategory.js
+++ pages/products/sortItemsByCategory.js
@@ -1,11 +1,12 @@
 import { getItems } from "./getItems.js";
 
 export function sortItemsByCategory() {
+  const proxy = "https://noroffcors.herokuapp.com/";
   const url = "http://megumikim.great-site.net/wp-json/wc/store/products/";
   const queryString = document.location.search;
   const params = new URLSearchParams(queryString);
   const categoryID = params.get("category");
-  const categoryUrl = url + `?category=${categoryID}`;
+  const categoryUrl = proxy + url + `?category=${categoryID}`;
 
   if (categoryID) {
     return getItems(categoryUrl);
