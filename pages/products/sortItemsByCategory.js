33be0c216fe946406e47dd0c6e99884682ec7c2a
Megumi Kimura <kimuramegumi@gmail.com>
2022-08-22 09:57:46 +0200

    cors fix

--- pages/products/sortItemsByCategory.js
+++ pages/products/sortItemsByCategory.js
@@ -3,13 +3,11 @@ import { getItems } from "./getItems.js";
 export function sortItemsByCategory() {
   const proxy = "https://noroffcors.herokuapp.com/";
   const url =
-    "https://mei-olivia.infinityfreeapp.com/wp-json/wc/store/products/";
+    // "https://mei-olivia.infinityfreeapp.com/wp-json/wc/store/products/";
+    "https://megumikim.great-site.net//wp-json/wc/store/products/";
+
   // "http://localhost/rainydays/wp-json/wc/store/products/";
 
-  // =======
-  //     // "https://mei-olivia.infinityfreeapp.com/wp-json/wc/store/products/";
-  //     "https://localhost/rainydays/wp-json/wc/store/products/";
-  // >>>>>>> Stashed changes
   const queryString = document.location.search;
   const params = new URLSearchParams(queryString);
   const categoryID = params.get("category");
