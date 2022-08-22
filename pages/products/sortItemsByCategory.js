df19904989c01597da599804c2cbc8d38d320d83
Megumi Kimura <kimuramegumi@gmail.com>
2022-08-22 09:41:49 +0200

    cors fix

--- pages/products/sortItemsByCategory.js
+++ pages/products/sortItemsByCategory.js
@@ -3,14 +3,13 @@ import { getItems } from "./getItems.js";
 export function sortItemsByCategory() {
   const proxy = "https://noroffcors.herokuapp.com/";
   const url =
-<<<<<<< Updated upstream
     "https://mei-olivia.infinityfreeapp.com/wp-json/wc/store/products/";
   // "http://localhost/rainydays/wp-json/wc/store/products/";
 
-=======
-    // "https://mei-olivia.infinityfreeapp.com/wp-json/wc/store/products/";
-    "https://localhost/rainydays/wp-json/wc/store/products/";
->>>>>>> Stashed changes
+  // =======
+  //     // "https://mei-olivia.infinityfreeapp.com/wp-json/wc/store/products/";
+  //     "https://localhost/rainydays/wp-json/wc/store/products/";
+  // >>>>>>> Stashed changes
   const queryString = document.location.search;
   const params = new URLSearchParams(queryString);
   const categoryID = params.get("category");
