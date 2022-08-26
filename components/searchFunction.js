2e7f241b5cad881171dbf4aa783db19f6b2f38b6
Megumi Kimura <kimuramegumi@gmail.com>
2022-08-26 23:24:42 +0200

    fix search function

--- components/searchFunction.js
+++ components/searchFunction.js
@@ -1,24 +1,23 @@
 import { getItems } from "../pages/products/getItems.js";
-// import { saveToStorage } from "../../constants/storage/local.js";
 
 export function searchFunction() {
   const searchButton = document.querySelector("#search_icon");
-  // const searchKey = "search";
+  const search = document.querySelector("#search");
   searchButton.onclick = function () {
+    search.style.display = "block";
     searchItems();
   };
 
-  document.addEventListener("keypress", function (event) {
+  search.addEventListener("keypress", function (event) {
     if (event.keyCode === 13) {
       event.preventDefault();
-      // saveToStorage("search", event.target.value);
       searchItems();
     }
   });
 }
 
 export function searchItems() {
-  const productsContainer = document.querySelector(".products-container");
+  const productsContainer = document.querySelector(".search_results");
   const url = "https://kimuramegumi.site/test/wp-json/wc/store/products/";
 
   let searchInput = document.querySelector("#search").value;
