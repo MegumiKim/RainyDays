d314b0e98b64e93846e4342d499019e10422a0b1
Megumi Kimura <kimuramegumi@gmail.com>
2023-03-09 13:31:38 +0100

    fix CORS fix

--- pages/products/sortItemsByCategory.js
+++ pages/products/sortItemsByCategory.js
@@ -1,13 +1,14 @@
 import { getItems } from "./getItems.js";
 
 export function sortItemsByCategory() {
-  const proxy = "https://noroffcors.herokuapp.com/";
+  const proxy = "https://noroffcors.onrender.com/";
   const url = "https://kimuramegumi.site/test/wp-json/wc/store/products/";
 
   const queryString = document.location.search;
   const params = new URLSearchParams(queryString);
   const categoryID = params.get("category");
   const categoryUrl = proxy + url + `?category=${categoryID}`;
+  console.log(categoryUrl);
   const productsContainer = document.querySelector(".products-container");
 
   if (categoryID) {
